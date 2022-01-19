8237793f6b26e237278f66fd57884fe001dae8e5
kaio <kaiohvs@gmail.com>
2022-01-19 13:17:45 -0300

    Criação do projeto pilha

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace EstruturaDoPrograma
+{
+    class Program
+    {
+        static void Main()
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(10);
+            s.Empilha(100);
+            System.Console.WriteLine(s.Desempilha());
+            System.Console.WriteLine(s.Desempilha());
+            System.Console.WriteLine(s.Desempilha());
+
+        }
+    }
+}
